441de00c4eb95d1b0e448265c4b49fa055deb20b
Jiří Eichler <eichler.jirka@gmail.com>
2018-08-22 21:40:51 +0200

    keystroke-counter ignore R-ctrl

--- keystroke-counter.c
+++ keystroke-counter.c
@@ -31,7 +31,7 @@ int main(int argc, char *argv[]) {
 	while (fread(&ev, sizeof(ev), 1, input)) {
 		if (ev.type == 1 && ev.value == 1 &&
 		ev.code != 42 && ev.code != 29 && ev.code != 125 && // L-shift, L-ctrl, L-win
-		ev.code != 56 && ev.code != 100 && ev.code != 54) { // L-alt, R-alt, R-shift
+		ev.code != 56 && ev.code != 100 && ev.code != 54 && ev.code != 97) { // L-alt, R-alt, R-shift, R-ctrl
 			count++;
 			printf ("%llu\n", count);
 		}
