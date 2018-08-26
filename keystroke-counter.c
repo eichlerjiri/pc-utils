ccd18d02c2dff023094a12ff00a3f67cc2d889aa
Jiří Eichler <eichler.jirka@gmail.com>
2018-08-26 02:19:49 +0200

    utilities to fix subtitles

--- keystroke-counter.c
+++ keystroke-counter.c
@@ -17,7 +17,8 @@ void fatal(char *msg, ...) {
 
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
-		fatal("Usage: %s /dev/input/<device>", argv[0]);
+		fprintf(stderr, "Usage: %s /dev/input/<device>\n", argv[0]);
+		return 1;
 	}
 
 	FILE *input = fopen(argv[1], "r");
